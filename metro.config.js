2259e12942b3cdf69c61c5036220ba77b83dee39
James Ide <ide@expo.dev>
2023-12-19 13:50:24 -0800

    Remove config that is now implied
    - Metro config has AVIF and HEIC now
    - Babel config now configures Expo Router
    - Don't need index.js, just use "main" in package.json
    
    Tested with `npx expo start -c`

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.assetExts.push('avif', 'heic');
-
-module.exports = config;
